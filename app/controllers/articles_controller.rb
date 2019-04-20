9681a66385bad426bd997591f16fd26a89b814b0
Dirius <cardozojoseignacio@gmail.com>
2019-04-20 15:55:26 -0300

    Clase 12 Strong params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,10 +13,7 @@ class ArticlesController < ApplicationController
    end
    #post /articles
    def create
-      @article=Article.new(
-         title:params[:article][:title],
-         body:params[:article][:body]
-      )
+      @article=Article.new(article_params)
       
       if @article.save
          redirect_to @article
@@ -28,5 +25,11 @@ class ArticlesController < ApplicationController
       @article= Article.find(params[:id])
       @article.destroy
       redirect_to articles_path
+   end
+
+   private
+
+   def article_params
+      params.require(:article).permit(:title,:body)
    end
 end
