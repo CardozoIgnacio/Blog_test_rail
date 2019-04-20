621cccd28c8732782dab37db315fb13090a1cae1
Dirius <cardozojoseignacio@gmail.com>
2019-04-19 22:05:39 -0300

    Clase8-Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   #get 'welcome/index'
+  resources :articles 
+  #
+  #  get "/articles"
+  #  post "/articles"
+  #  delet "/articles"
+  #  get "/articles/:id"
+  #  get "articles/:new"
+  #  get "articles/:id/edit"
+  #  path "articles/:id"
+  #  post "/articles/:id"
+  #
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
