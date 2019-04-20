0884605de8baa0a1812a5a51b6d31bcfcd80a810
Dirius <cardozojoseignacio@gmail.com>
2019-04-20 14:05:57 -0300

    Clase 10 Validacion datos de articulo v2

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord 
-    validates :title, presence:true
-    validates :body, presence:true
+    validates :title, presence:true,uniqueness:true
+    validates :body, presence:true,length: {minimum:20}
 
 
 end
