4b52369510cbc5d40393f7d7096f963714bf6181
Dirius <cardozojoseignacio@gmail.com>
2019-04-19 23:03:07 -0300

    Video 9 enviar datos al controlador

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,23 @@
 class ArticlesController < ApplicationController
-    
+    #get /articles
     def index
        @articles = Article.all
     end
-
+    #get /articles/id
     def show
        @article = Article.find(params[:id])
     end
-
+    #get /articles/new
     def new
       @article = Article.new
+   end
+   #post /articles
+   def create
+      @article=Article.new(
+         title:params[:article][:title],
+         body:params[:article][:body]
+      )
+      @article.save
+      
    end
 end
