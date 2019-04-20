bb51ab841d4b4d49f47d1804491ca0d865f37286
Dirius <cardozojoseignacio@gmail.com>
2019-04-20 16:40:45 -0300

    Clase 13 Vistas parciales

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,22 @@ class ArticlesController < ApplicationController
       redirect_to articles_path
    end
 
+   def edit
+      @article = Article.find(params[:id])
+   
+   end
+
+   def update
+      @article = Article.find(params[:id])
+
+      if@article.update(article_params)
+         redirect_to @article
+      else
+         render :edit
+      end
+   end
+   
+
    private
 
    def article_params
